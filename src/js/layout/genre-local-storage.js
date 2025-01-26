19d27f773fca93491243b91bc5102db1e0148044
Falconoff <sokolovserega1982@gmail.com>
2025-01-26 22:51:38 +0200

    fix: youtube icon & first render

--- src/js/layout/genre-local-storage.js
+++ src/js/layout/genre-local-storage.js
@@ -1,16 +1,14 @@
-import { getApiData } from '../api/api-service'
-import { setToLocalStorage, getFromLocalStorage } from './local-storage'
-
+import { getApiData } from '../api/api-service';
+import { setToLocalStorage, getFromLocalStorage } from './local-storage';
 
 export function setGenresToLocalStorage() {
   let query = `genre/movie/list?`;
-  getApiData(query)
-    .then(result => {
-      setToLocalStorage('genres', result)
-    });
+  getApiData(query).then(result => {
+    setToLocalStorage('genres', result);
+  });
 }
-setGenresToLocalStorage()
+setGenresToLocalStorage();
 
 export function getGenresFromLocalStorage() {
-  return getFromLocalStorage('genres').genres
+  return getFromLocalStorage('genres')?.genres;
 }
